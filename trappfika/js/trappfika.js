8f8c6df9c6c39d7fea3846371d3376de67576b9b
Johan Gustafson <johan.gustafson@gmail.com>
2013-11-19 23:51:54 +0100

    Improve how points update
    - Update points on mouse leave on the row
    - Color changed contestants for some time after change

--- trappfika/js/trappfika.js
+++ trappfika/js/trappfika.js
@@ -1,46 +1,66 @@
 
-var myapp = angular.module('trappfika', ['ngRoute', 'firebase']).
-    config(function($routeProvider) {
+var trappfika = angular.module('trappfika', ['ngRoute', 'firebase']);
+trappfika.value('firebaseUrl', 'https://trappfika.firebaseio.com/test');
+trappfika.config(function($routeProvider) {
     $routeProvider.
         when('/', {templateUrl:'list.html'}).
         when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
         otherwise({redirectTo:'/'});
 });
 
-myapp.controller('MyCtrl', ['$scope', 'angularFire',
-    function MyCtrl($scope, angularFire, fbURL) {
-        var ref = new Firebase('https://trappfika.firebaseio.com/contestants');
+trappfika.controller('ListCtrl', ['$scope', 'angularFire', 'firebaseUrl', '$timeout',
+    function($scope, angularFire, firebaseUrl, $timeout) {
+        var ref = new Firebase(firebaseUrl);
         angularFire(ref, $scope, 'contestants', []);
+        $scope.origCopys = {};
 
+        function updatePoints(orig, updated) {
+            if(!equalPoints(orig, updated)) {
+                updated.points = parseInt(updated.up) * 2 + parseInt(updated.down);
+                flashColor(updated);
+            }
+        }
 
-        function calcPoints(contestant) {
-            contestant.points = parseInt(contestant.up) * 2 + parseInt(contestant.down);
+        function equalPoints(orig, current) {
+            return (orig.up == current.up && orig.down == current.down);
+        }
+
+        function flashColor(contestant) {
+            contestant.updated = true;
+            $timeout(function() {
+                contestant.updated = false;
+            }, 500);
+        }
+
+        $scope.enter = function(contestant) {
+            $scope.origCopys[contestant.name] = angular.copy(contestant);
+        }
+
+        $scope.leave = function(contestant) {
+            updatePoints($scope.origCopys[contestant.name], contestant);
+            delete $scope.origCopys[contestant.name];
         }
 
         $scope.addUp = function(contestant) {
             contestant.up = parseInt(contestant.up) + 1;
-            calcPoints(contestant);
         }
 
         $scope.addDown = function(contestant) {
             contestant.down = parseInt(contestant.down) + 1;
-            calcPoints(contestant);
         }
 
         $scope.subtractUp = function(contestant) {
             contestant.up = parseInt(contestant.up) - 1;
-            calcPoints(contestant);
         }
 
         $scope.subtractDown = function(contestant) {
             contestant.down = parseInt(contestant.down) - 1;
-            calcPoints(contestant);
         }
     }
 ]);
 
-function CreateCtrl($scope, $location, angularFire) {
-    var ref = new Firebase('https://trappfika.firebaseio.com/contestants');
+function CreateCtrl($scope, $location, angularFire, firebaseUrl) {
+    var ref = new Firebase(firebaseUrl);
     angularFire(ref, $scope, 'contestants', []);
 
     $scope.save = function() {
